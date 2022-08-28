44a99181ea5aab8c6f4782dc3de6f0e8258779c2
AJ Phoenix <ajph03nix@gmail.com>
2022-08-28 19:02:55 -0400

    attempted to fix empty [0][0] if the triangle

--- function-practice-4.py
+++ function-practice-4.py
@@ -19,35 +19,40 @@ def num_within(num,low,high):
 def pascal(num):
     triangle = [[]]#[[1],[1,1]]
     prev_row = triangle[0]
-    row = []
     i = 0
     if num < 1:
         print("Invalid Input  -|😳|-")
-    elif num == 1:
-        prev_row = triangle[0]
-    elif num == 2:
-        prev_row = triangle[1]
-
     else:
-        for i in range(num):
-
-            for c in range(len(str(prev_row))):
-                if (c == 0) or (c == i):
-                    row.append(1)
-                if ((c)>0) and (c<i):
-                    row.append(prev_row[c-1] + prev_row[c])
+        for i in range(num+1):
+            row = []
+            if num == 1:
+                prev_row = triangle[0]
+            # elif num == 2:
+            #     prev_row = triangle[1]
+            #     print(f"prev_row {prev_row}")
+            else:
+                for c in range(len(str(prev_row))):
+                    if (c == 0) or (c == i):
+                        row.append(1)
+                    if ((c)>0) and (c<i):
+                        row.append(prev_row[c-1] + prev_row[c])
             prev_row = row
             row = []
             triangle.append(prev_row)
             print(triangle[i])
     # return triangle
 
-print(max_num(1,4,5))
-print(mult_list([1,4,5,3]))
-print(rev_string("1,4,5"))
+
+print("[1,4,5]")
+print(f"max num: {max_num(1,4,5)}")
+print("[1,4,5,3]")
+print(f"multiplied list: {mult_list([1,4,5,3])}")
+rev =  rev_string("1,4,5")
+print("[1,4,5]")
+print(f"reversed string: {rev}")
 print(num_within(1,2,3))
 print(num_within(3,2,4))
-# print(pascal(0))
+print(pascal(0))
 print(pascal(5))
 print(pascal(20))
 
